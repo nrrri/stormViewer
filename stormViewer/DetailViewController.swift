f3e0b9dc67be7b7cf55ea2405bef1850473438de
Ferin.Chow <nrrri.cho@gmail.com>
2024-06-25 00:22:48 -0400

    first commit

---
+++ stormViewer/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  stormViewer
+//
+//  Created by Narissorn Chowarun on 2024-06-24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
